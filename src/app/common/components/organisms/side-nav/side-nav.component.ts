0e8bbe48103d03ba17eba89aa97d98508666b697
crossoverV0 <crossoverv0@gmail.com>
2023-04-23 17:51:40 -0300

    iniciando itens do menu de navegação

--- src/app/common/components/organisms/side-nav/side-nav.component.ts
+++ src/app/common/components/organisms/side-nav/side-nav.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const NAV_ITEMS = [
+  {
+    title: 'Tarefas',
+    class: 'bx bx-task bx-sm v-icon'
+  },
+  {
+    title: 'Bugs',
+    class: 'bx bx-bug-alt bx-sm v-icon'
+  },
+  {
+    title: 'Notificações',
+    class: 'bx bx-bell bx-sm v-icon'
+  }
+]
+
 @Component({
   selector: 'app-side-nav',
   standalone: true,
@@ -9,5 +24,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
-
+  navItems: any[] = NAV_ITEMS
 }
--- src/app/pages/features/current-tasks/current-tasks.component.ts
+++ src/app/pages/features/current-tasks/current-tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ThemeService } from 'src/app/common/services/theme.service';
 
 @Component({
   selector: 'app-current-tasks',
@@ -9,5 +10,22 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./current-tasks.component.scss']
 })
 export class CurrentTasksComponent {
+  constructor(protected themeService: ThemeService) {}
 
+  ngOnInit(): void {
+    this.themeService.set('dark')
+  }
+
+  show(){
+    const currentTheme = this.themeService.activeTheme
+    if(currentTheme == 'dark'){
+      this.switchTheme('light')
+    }else{
+      this.switchTheme('dark')
+    }
+  }
+
+  switchTheme(themeName: string){
+    this.themeService.set(`${themeName}`)
+  }
 }
